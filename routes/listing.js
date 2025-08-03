1670ccf63e6e228ff9b4ea6cc511a4ac38ecaee1
raunaksomani25 <156603817+raunaksomani25@users.noreply.github.com>
2025-08-03 20:36:36 +0530

    Edge case error fixed

--- routes/listing.js
+++ routes/listing.js
@@ -29,7 +29,7 @@ router.get("/search", wrapAsync(async (req, res) => {
     });
   }
 
-  res.render("listings/index", { allListings: listings });
+  res.render("listings/index", { allListings: listings,, category: null });
 }));
  
 router.route("/:id")
