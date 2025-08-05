1653b42cb43e336ee2fb55631add9676660d2071
raunaksomani25 <156603817+raunaksomani25@users.noreply.github.com>
2025-08-05 08:23:58 +0530

    Created booking.js

---
+++ models/booking.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema({
+  listing: {
+    type: Schema.Types.ObjectId,
+    ref: "Listing",
+    required: true
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  checkIn: {
+    type: Date,
+    required: true
+  },
+  checkOut: {
+    type: Date,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Booking", bookingSchema);
