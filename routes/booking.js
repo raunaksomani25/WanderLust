6cff2e64503c639d92d886af50aa4cbe14d420d9
raunaksomani25 <156603817+raunaksomani25@users.noreply.github.com>
2025-08-05 08:54:50 +0530

    Update booking.js

--- routes/booking.js
+++ routes/booking.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const bookings = require("../controllers/booking.js");
 const { isLoggedIn } = require("../middleware");
+const wrapAsync=require("../utils/wrapAsync.js");
 
-router.post("/listings/:id/book", isLoggedIn, bookings.createBooking);
-router.get("/my/bookings", isLoggedIn,bookings.viewBooking);
-router.delete("/bookings/:id", isLoggedIn, bookings.deleteBooking);
-router.get("/bookings/:id/edit", isLoggedIn,bookings.editBooking);
-router.put("/bookings/:id",isLoggedIn,bookings.updateBooking);
+router.post("/listings/:id/book", isLoggedIn, wrapAsync(bookings.createBooking));
+router.get("/my/bookings", isLoggedIn,wrapAsync(bookings.viewBooking));
+router.delete("/bookings/:id", isLoggedIn, wrapAsync(bookings.deleteBooking));
+router.get("/bookings/:id/edit", isLoggedIn,wrapAsync(bookings.editBooking));
+router.put("/bookings/:id",isLoggedIn,wrapAsync(bookings.updateBooking));
 
 
 module.exports = router;
